0a1f03b7e22608ed6611ff4dc1098f51ceb6480f
genievy <e.potrakhov@gmail.com>
2022-08-06 19:06:26 +0300

    fix_create_branch_fault

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -19,7 +19,7 @@ class TextBoxPage(BasePage):
         self.element_is_visible(self.locators1.LAST_NAME).send_keys('Kuznetsov')
         self.element_is_visible(self.locators1.ADDRESS_1).send_keys('Gogolya, 7')
         self.element_is_visible(self.locators1.ADDRESS_2).send_keys('Gogolya, 7')
-        self.element_is_visible(self.locators1.POSTAL_CODE).send_keys('109112')
+        self.element_is_visible(self.locators1.POSTAL_CODE).send_keys('176789')
         self.element_is_visible(self.locators1.CITY).send_keys('Moscow')
         # self.element_is_visible(self.locators1.COUNTRY_SELECT).click()
         # self.element_is_visible(self.locators1.COUNTRY_OPTION).click()
@@ -27,16 +27,20 @@ class TextBoxPage(BasePage):
         self.element_is_visible(self.locators1.EMAIL).send_keys('qwe@qwe.qwe')
         self.element_is_visible(self.locators1.PHONE).send_keys('+79453456789')
         self.element_is_visible(self.locators1.DESIRED_PASSWORD).send_keys('auSrL3iW3wzCkRk')
-        time.sleep(3)
         self.element_is_presence(self.locators1.CONFIRM_PASSWORD).send_keys('auSrL3iW3wzCkRk')
         self.element_is_visible(self.locators1.CAPTCHA).send_keys('5432')
         self.element_is_visible(self.locators2.CHECKBOX_1).click()
         self.element_is_visible(self.locators2.CHECKBOX_2).click()
-        self.element_is_visible(self.locators3.CREATE_ACCOUNT_BUTTON).click()
+        self.go_to_element(self.locators3.CREATE_ACCOUNT_BUTTON)
+        self.element_is_presence(self.locators3.CREATE_ACCOUNT_BUTTON).click()
+        # self.alert_is_present()
+        time.sleep(1)
 
-        if self.find_element(self.locators4.CAPTCHA_ALERT).get_attribute('textContent') == "\n  \n   Invalid CAPTCHA given\n":
-            print("Invalid CAPTCHA given")
-        else:
+
+    def check_alert(self):
+        try:
+            text_alert = self.find_captcha(self.locators4.CAPTCHA_ALERT).get_attribute('textContent')
+            if text_alert.strip() == "Invalid CAPTCHA given":
+                print(text_alert.strip())
+        except:
             print("Invalid input data")
-
-        time.sleep(25)
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,3 @@
-
-# from pages.base_page import BasePage
-# from locators.elements_page_locators import TextBoxPageLocators
 from pages.elements_page import TextBoxPage
 
 
@@ -11,12 +8,9 @@ class TestElements:
             text_box_page = TextBoxPage(driver, 'http://localhost/litecart/create_account')
             text_box_page.open()
             text_box_page.fill_textboxes()
+            text_box_page.check_alert()
 
-
-# def test(driver):
-#     page = BasePage(driver, 'http://localhost/litecart/create_account')
-#     locator_1 = TextBoxPageLocators()
-#     page.open()
-#     page.element_is_visible(locator_1.COMPANY)
-#     time.sleep(5)
-
+        # def test_input_proposed_password(self, driver):
+        #     text_box_page = TextBoxPage(driver, 'http://localhost/litecart/create_account')
+        #     text_box_page.open()
+        #     text_box_page.
