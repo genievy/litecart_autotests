62b3d94ed7d02f128475e43df746fd7d3cafd86a
genievy <e.potrakhov@gmail.com>
2022-08-09 00:30:12 +0300

    add_check_filled_form; changes in elements_test.py, elements_page.py

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -7,7 +7,6 @@ class TextBoxPage(BasePage):
     locators = TextBoxPageLocators()
 
     def fill_textboxes(self):
-        self.element_is_visible(self.locators.COOKIES).click()
         self.element_is_visible(self.locators.COMPANY).send_keys('Company')
         self.element_is_visible(self.locators.TAX_ID).send_keys('2346556')
         self.element_is_visible(self.locators.FIRST_NAME).send_keys('Oleg')
@@ -24,10 +23,29 @@ class TextBoxPage(BasePage):
         self.action_release(self.element_is_visible(self.locators.DESIRED_PASSWORD))
         self.element_is_visible(self.locators.DESIRED_PASSWORD).send_keys('auSrL3iW3wzCkRk')
         self.element_is_presence(self.locators.CONFIRM_PASSWORD).send_keys('auSrL3iW3wzCkRk')
-        # self.element_is_visible(self.locators.CAPTCHA).send_keys('5432')
+        self.element_is_visible(self.locators.CAPTCHA).send_keys('5432')
         self.element_is_visible(self.locators.CHECKBOX_1).click()
         self.element_is_visible(self.locators.CHECKBOX_2).click()
         self.go_to_element(self.element_is_visible(self.locators.CREATE_ACCOUNT_BUTTON))
-        print(self.element_is_visible(self.locators.CREATE_ACCOUNT_BUTTON))
-        self.element_is_presence(self.locators.CREATE_ACCOUNT_BUTTON).click()
+        # print(self.element_is_visible(self.locators.CREATE_ACCOUNT_BUTTON))
+        # self.element_is_presence(self.locators.CREATE_ACCOUNT_BUTTON).click()
         time.sleep(3)
+
+    def check_filled_form(self):
+        company = self.element_is_visible(self.locators.COMPANY).get_attribute('value')
+        tax_id = self.element_is_visible(self.locators.TAX_ID).get_attribute('value')
+        first_name = self.element_is_visible(self.locators.COMPANY).get_attribute('value')
+        last_name = self.element_is_visible(self.locators.LAST_NAME).get_attribute('value')
+        adress1 = self.element_is_visible(self.locators.ADDRESS_1).get_attribute('value')
+        adress2 = self.element_is_visible(self.locators.ADDRESS_2).get_attribute('value')
+        postal_code = self.element_is_visible(self.locators.POSTAL_CODE).get_attribute('value')
+        city = self.element_is_visible(self.locators.CITY).get_attribute('value')
+        email = self.element_is_visible(self.locators.EMAIL).get_attribute('value')
+        phone = self.element_is_visible(self.locators.PHONE).get_attribute('value')
+        desired_password = self.element_is_visible(self.locators.DESIRED_PASSWORD).get_attribute('value')
+        confirm_password = self.element_is_visible(self.locators.CONFIRM_PASSWORD).get_attribute('value')
+        captcha = self.element_is_visible(self.locators.CAPTCHA).get_attribute('value')
+        checkbox_1 = self.element_is_visible(self.locators.CHECKBOX_1).get_attribute('checked')
+        checkbox_2 = self.element_is_visible(self.locators.CHECKBOX_2).get_attribute('checked')
+
+        return company, tax_id, first_name, last_name, adress1, adress2, postal_code, email, city, phone, desired_password, confirm_password, captcha, checkbox_1, checkbox_2
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -8,6 +8,9 @@ class TestElements:
             text_box_page = TextBoxPage(driver, 'http://localhost/litecart/create_account')
             text_box_page.open()
             text_box_page.fill_textboxes()
+            text_box_page.check_filled_form()
+            print(text_box_page.check_filled_form())
+
 
         # def test_input_proposed_password(self, driver):
         #     text_box_page = TextBoxPage(driver, 'http://localhost/litecart/create_account')
